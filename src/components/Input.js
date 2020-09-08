daa7d038b3be5acaced92062025e5188d3a8240d
Suzanne Cabral <suzannecabral@live.com>
2020-09-08 07:21:29 -0700

    Input widget complete

--- src/components/Input.js
+++ src/components/Input.js
@@ -57,13 +57,14 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    textTransform:'uppercase',
+    color: inputValue.length < 10 ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue}</div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} value={inputValue}/> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
