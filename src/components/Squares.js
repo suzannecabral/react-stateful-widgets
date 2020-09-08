9b7d026c1c129a23418d4bebaff5930a781ee2f6
Suzanne Cabral <suzannecabral@live.com>
2020-09-08 08:05:36 -0700

    Squares markActive works, working on getClassName

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,16 +14,21 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
+
+
 
 export default function Squares() {
   // Use the state hook twice, as we need two slices of state: 'squares' and
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
+
+  const [squares, setSquares] = useState(listOfSquareIds);
+  const [activeSquare, setActiveSquare] = useState()
 
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
@@ -38,6 +43,9 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+    activeSquare === id
+    ? setActiveSquare('') 
+    : setActiveSquare(id)
   };
 
   return (
@@ -48,7 +56,7 @@ export default function Squares() {
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
